c766a48b02ec4579d45f935cbcdac1d0693ac215
jahidhasan8 <jahidhasan1932@gmail.com>
2022-10-18 12:58:30 +0600

    basic backend server for dragon news

---
+++ index.js
@@ -0,0 +1,23 @@
+
+const express=require('express')
+
+const app=express();
+const cors=require('cors');
+const port=process.env.port ||5000;
+
+
+app.use(cors());
+const categories=require('./data/categories.json')
+
+app.get('/',(req,res)=>{
+    res.send('News api running');
+    
+    });
+
+app.get('/news-categories',(req,res)=>{
+    res.send(categories)
+})
+    app.listen(port,()=>{
+        console.log('narnia news news server running on port',port)
+
+})
